1628c06228107d725af6e3cfe623ed4c447fe527
Omer Coskun <o.csknn@gmail.com>
2026-06-11 10:09:52 +0200

    feat: GA4 en Microsoft Clarity tracking toegevoegd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,28 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           as="image"
           type="image/webp"
         />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YZJ22GGTS4"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-YZJ22GGTS4');
+            `,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "wnxdne8bn4");
+            `,
+          }}
+        />
       </head>
       <body className="font-[var(--font-inter)] antialiased bg-white text-gray-900">
         <Navbar />
